7e0e7a164f40644d3ac4762d4b20b539c8bf2cf1
yang <59786677@qq.com>
2019-05-16 00:38:11 +0800

    first commit

---
+++ visualization/extract_log.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# coding=utf-8
+# 该文件用来提取训练log，去除不可解析的log后使log文件格式化，生成新的log文件供可视化工具绘图
+
+import inspect
+import os
+import random
+import sys
+
+
+def extract_log(log_file, new_log_file, key_word):
+    with open(log_file, 'r') as f:
+        with open(new_log_file, 'w') as train_log:
+            # f = open(log_file)
+            # train_log = open(new_log_file, 'w')
+            for line in f:
+                # 去除多gpu的同步log
+                if 'Syncing' in line:
+                    continue
+                # 去除除零错误的log
+                if 'nan' in line:
+                    continue
+                if key_word in line:
+                    train_log.write(line)
+    f.close()
+    train_log.close()
+
+
+extract_log('train_yolov3.log_0512_5', 'log_0512_5_loss.txt', 'images')
+extract_log('train_yolov3.log_0512_5', 'log_0512_5_IOU.txt', 'IOU')
+
+# extract_log('log_2019-05-10', 'train_log_loss.txt', 'images')
+# extract_log('log_2019-05-10', 'train_log_iou.txt', 'IOU')
---
+++ visualization/train_loss_visualization.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %matplotlib inline
+
+lines = 0  # 改为自己生成的train_log_loss.txt中的行数
+result = pd.read_csv('log_0512_5_loss.txt', skiprows=[x for x in range(lines) if ((x % 10 != 9)| x<=1000)],
+                     error_bad_lines=False, names=['loss', 'avg', 'rate', 'seconds', 'images'])
+# result = pd.read_csv('train_log_loss.txt', skiprows=[x for x in range(lines) if (x % 10 != 9) ],
+#                      error_bad_lines=False, names=['loss', 'avg', 'rate', 'seconds', 'images'])
+result.head()
+
+result['loss'] = result['loss'].str.split(' ').str.get(1)
+result['avg'] = result['avg'].str.split(' ').str.get(1)
+result['rate'] = result['rate'].str.split(' ').str.get(1)
+result['seconds'] = result['seconds'].str.split(' ').str.get(1)
+result['images'] = result['images'].str.split(' ').str.get(1)
+result.head()
+result.tail()
+
+print(result.head())
+# print(result.tail())
+# print(result.dtypes)
+
+# print(result['loss'])
+# print(result['avg'])
+# print(result['rate'])
+# print(result['seconds'])
+# print(result['images'])
+
+result['loss'] = pd.to_numeric(result['loss'])
+result['avg'] = pd.to_numeric(result['avg'])
+result['rate'] = pd.to_numeric(result['rate'])
+result['seconds'] = pd.to_numeric(result['seconds'])
+result['images'] = pd.to_numeric(result['images'])
+# result.dtypes
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+# ax.plot(result['avg'].values, label='avg_loss')
+ax.plot(result['loss'].values[1000:],label='loss')
+
+xtick = np.arange(0,11000,1000)
+ax.legend(loc='best')  # 图列自适应位置
+ax.set_title('The loss curves')
+ax.set_xlabel('batches')
+plt.xticks(xtick)
+plt.xticks(xtick, xtick+1000)
+# plt.xlim([900,10000])
+# fig.savefig('avg_loss')
+fig.savefig('loss')
+plt.show()
